34f8247734eaae334652f0b3458ec6a3e7362147
zhangfuwen <zhangfuwen@foxmail.com>
2025-08-07 00:20:50 +0800

    update scripts for install nerdfonts

--- zsh/.zsh.d/tools.zsh
+++ zsh/.zsh.d/tools.zsh
@@ -4,15 +4,38 @@ install_oh_my_zsh() {
 }
 function install_nerdfonts() {
   mkdir -p ~/bin/src/nerdfonts
-  mkdir ~/.fonts
 
-  wget -O ~/bin/src/nerdfonts/FireCode.zip \
-    https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-  unzip ~/bin/src/nerdfonts/FireCode.zip -d ~/.fonts/
-  echo "now you can set your terminal fonts to 'FiraCode Nerd Font Mono Regular'"
+  os_kernel = $(uname -s)
+  # Darwin or Linux
+  install_target="~/.fonts"
+  if [[ $os_kernel == "Darwin" ]]; then
+      install_target = "~/Library/Fonts"
+  elif [[ $os_kernel == "Linux" ]]; then
+      install_target="~/.fonts"
+  else
+      echo "unsupported os"
+  fi
 
-  wget -O ~/bin/src/nerdfonts/RobotoMono.zip \
-    https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip
-  unzip ~/bin/src/nerdfonts/RobotoMono.zip -d ~/.fonts/
-  echo "now you can set your terminal fonts to 'RobotoMono Regular'"
+  if [[ ! -d $install_target ]]; then
+      mkdir -p $install_target
+  fi
+
+
+  if [[ ! -f ~/bin/src/nerdfonts/FireCode.zip ]]; then
+      wget -c -O ~/bin/src/nerdfonts/FireCode.zip \
+        https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+      unzip ~/bin/src/nerdfonts/FireCode.zip -d $install_target
+      echo "now you can set your terminal fonts to 'FiraCode Nerd Font Mono Regular'"
+  else
+      echo "FireCode font exists"
+  fi
+
+  if [[ ! -f ~/bin/src/nerdfonts/RobotoMono.zip ]]; then
+      wget -c -O ~/bin/src/nerdfonts/RobotoMono.zip \
+            https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip
+      unzip ~/bin/src/nerdfonts/RobotoMono.zip -d $install_target
+      echo "now you can set your terminal fonts to 'RobotoMono Regular'"
+  else
+      echo "RobotoMono font exists"
+  fi
 }
