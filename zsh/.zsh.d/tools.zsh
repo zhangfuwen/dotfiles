f24d98b8dde2da1d8819dca72225fedcb821f3b3
zhangfuwen <zhangfuwen@foxmail.com>
2025-09-30 12:22:38 +0800

    add font CommicShannsMono

--- zsh/.zsh.d/tools.zsh
+++ zsh/.zsh.d/tools.zsh
@@ -5,6 +5,8 @@ install_oh_my_zsh() {
 function install_nerdfonts() {
   mkdir -p ~/bin/src/nerdfonts
 
+  font_list="FireCode.zip RobotoMono.zip ComicShannsMono"
+
   os_kernel = $(uname -s)
   # Darwin or Linux
   install_target="~/.fonts"
@@ -20,22 +22,10 @@ function install_nerdfonts() {
       mkdir -p $install_target
   fi
 
-
-  if [[ ! -f ~/bin/src/nerdfonts/FireCode.zip ]]; then
-      wget -c -O ~/bin/src/nerdfonts/FireCode.zip \
-        https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-      unzip ~/bin/src/nerdfonts/FireCode.zip -d $install_target
-      echo "now you can set your terminal fonts to 'FiraCode Nerd Font Mono Regular'"
-  else
-      echo "FireCode font exists"
-  fi
-
-  if [[ ! -f ~/bin/src/nerdfonts/RobotoMono.zip ]]; then
-      wget -c -O ~/bin/src/nerdfonts/RobotoMono.zip \
-            https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip
-      unzip ~/bin/src/nerdfonts/RobotoMono.zip -d $install_target
-      echo "now you can set your terminal fonts to 'RobotoMono Regular'"
-  else
-      echo "RobotoMono font exists"
-  fi
+  for font in $font_list; do
+      if [[ ! -f ~/bin/src/nerdfonts/$font ]]; then
+          wget -c -O ~/bin/src/nerdfonts/$font \
+            https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/$font
+      fi
+  done
 }
